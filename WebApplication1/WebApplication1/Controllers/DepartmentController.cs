ec407e0bc788b81f6fea66dfbe9a79d2ffd8d8fe
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-09-30 15:55:06 -0400

    Added DepartmentController

---
+++ WebApplication1/WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select DepartmentId, DepartmentName 
+                            from dbo.Department
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using(SqlCommand command=new SqlCommand(query, sqlConnection))
+                {
+                    sqlDataReader = command.ExecuteReader();
+                    table.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            string query = @"
+                            insert into dbo.Department
+                            values(@DepartmentName)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand(query, sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                    sqlDataReader = command.ExecuteReader();
+                    table.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department department)
+        {
+            string query = @"
+                            update dbo.Department
+                            set DepartmentName = @DepartmentName
+                            where DepartmentId = @DepartmentId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand(query, sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                    command.Parameters.AddWithValue("@DepartmentId", department.DepartmentId);
+                    sqlDataReader = command.ExecuteReader();
+                    table.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Department
+                            where DepartmentId = @DepartmentId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand(query, sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@DepartmentId", id);
+                    sqlDataReader = command.ExecuteReader();
+                    table.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
