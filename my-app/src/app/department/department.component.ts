82c0c1b1b2067842e8b8e4ebbb914cbb9e2b8ab2
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-09-30 20:42:22 -0400

    Added Modal Popup Window

--- my-app/src/app/department/department.component.ts
+++ my-app/src/app/department/department.component.ts
@@ -12,6 +12,9 @@ export class DepartmentComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   departments: any = [];
+  modalTitle = '';
+  DepartmentId = 0;
+  DepartmentName = '';
 
   ngOnInit(): void {
     this.refreshList();
@@ -21,5 +24,51 @@ export class DepartmentComponent implements OnInit {
     this.http.get<any>(environment.API_URL + 'department').subscribe((data) => {
       this.departments = data;
     });
+  }
+
+  addClick() {
+    this.modalTitle = 'Add Department';
+    this.DepartmentId = 0;
+    this.DepartmentName = '';
+  }
+
+  editClick(dep: { DepartmentId: number; DepartmentName: string }) {
+    this.modalTitle = 'Edit Department';
+    this.DepartmentId = dep.DepartmentId;
+    this.DepartmentName = dep.DepartmentName;
+  }
+
+  createClick() {
+    const body = { DepartmentName: this.DepartmentName };
+
+    this.http
+      .post(environment.API_URL + 'department', body)
+      .subscribe((res) => {
+        alert(res.toString());
+        this.refreshList();
+      });
+  }
+
+  updateClick() {
+    const body = {
+      DepartmentId: this.DepartmentId,
+      DepartmentName: this.DepartmentName,
+    };
+
+    this.http.put(environment.API_URL + 'department', body).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id: number) {
+    if (confirm('Are you sure?')) {
+      this.http
+        .delete(environment.API_URL + 'department/' + id)
+        .subscribe((res) => {
+          alert(res.toString());
+          this.refreshList();
+        });
+    }
   }
 }
