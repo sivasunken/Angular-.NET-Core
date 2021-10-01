7a67d4974426745da1c28a50e35c4bf0e2fe34b8
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-09-30 21:18:00 -0400

    Added Employee Details Screen

--- my-app/src/app/department/department.component.ts
+++ my-app/src/app/department/department.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
-import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-department',
--- my-app/src/app/employee/employee.component.ts
+++ my-app/src/app/employee/employee.component.ts
@@ -1,15 +1,101 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  employees: any = [];
+  departments: any = [];
+
+  modalTitle = '';
+  EmployeeId = 0;
+  EmployeeName = '';
+  Department = '';
+  DateOfJoining = '';
+  PhotoFileName = 'anonymous.png';
+  PhotoPath = environment.PHOTO_URL;
 
   ngOnInit(): void {
+    this.refreshList();
   }
 
+  refreshList() {
+    this.http.get<any>(environment.API_URL + 'employee').subscribe((data) => {
+      this.employees = data;
+    });
+
+    this.http.get<any>(environment.API_URL + 'department').subscribe((data) => {
+      this.departments = data;
+    });
+  }
+
+  addClick() {
+    this.modalTitle = 'Add Employee';
+    this.EmployeeId = 0;
+    this.EmployeeName = '';
+    this.Department = '';
+    this.DateOfJoining = '';
+    this.PhotoFileName = 'anonymous.png';
+  }
+
+  editClick(emp: {
+    EmployeeId: number;
+    EmployeeName: string;
+    Department: string;
+    DateOfJoining: string;
+    PhotoFileName: string;
+  }) {
+    this.modalTitle = 'Edit Employee';
+    this.EmployeeId = emp.EmployeeId;
+    this.EmployeeName = emp.EmployeeName;
+    this.Department = emp.Department;
+    this.DateOfJoining = emp.DateOfJoining;
+    this.PhotoFileName = emp.PhotoFileName;
+  }
+
+  createClick() {
+    const body = {
+      EmployeeName: this.EmployeeName,
+      Department: this.Department,
+      DateOfJoining: this.DateOfJoining,
+      PhotoFileName: this.PhotoFileName,
+    };
+
+    this.http.post(environment.API_URL + 'employee', body).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  updateClick() {
+    const body = {
+      EmployeeId: this.EmployeeId,
+      EmployeeName: this.EmployeeName,
+      Department: this.Department,
+      DateOfJoining: this.DateOfJoining,
+      PhotoFileName: this.PhotoFileName,
+    };
+
+    this.http.put(environment.API_URL + 'employee', body).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id: number) {
+    if (confirm('Are you sure?')) {
+      this.http
+        .delete(environment.API_URL + 'employee/' + id)
+        .subscribe((res) => {
+          alert(res.toString());
+          this.refreshList();
+        });
+    }
+  }
 }
