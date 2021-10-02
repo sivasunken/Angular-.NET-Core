8bcaa4df42985fd780712576bcc6323b2d0d9264
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-10-01 21:28:32 -0400

    Added Mongo Models

---
+++ WebApplication1/WebApplication1/Models/Mongo/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+
+namespace WebApplication1.Models.Mongo
+{
+    public class Department
+    {
+        public ObjectId Id { get; set; }
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Mongo/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+
+namespace WebApplication1.Models.Mongo
+{
+    public class Employee
+    {
+        public ObjectId Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Department { get; set; }
+
+        public string DateOfJoining { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
